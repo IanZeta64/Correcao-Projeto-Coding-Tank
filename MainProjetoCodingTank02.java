e0c5f51143bd3644b59d059a5ba767f99454fb93
Ian Paschoal <104800595+IanZeta64@users.noreply.github.com>
2022-10-23 14:40:16 -0300

    Update MainProjetoCodingTank02.java

--- MainProjetoCodingTank02.java
+++ MainProjetoCodingTank02.java
@@ -15,37 +15,49 @@ public class MainProjetoCodingTank02 {
         char saidaT = ' ';
         Double[] temperatura;
         Double[] convertida;
-try {
-    System.out.println("Digite a quantidade de entradas as serem transformadas:");
-    n = sc.nextInt();
-    System.out.println("Digite o tipo de temperatura para ser convertido(1-C/2-F/3-K): ");
-    entradaT = sc.next().charAt(0);
-    System.out.println("Digite o tipo de temperatura para qual deseja converter(1-C/2-F/3-K): ");
-    saidaT = sc.next().charAt(0);
-} catch(Throwable e){
-    System.out.println("erro do tipo " + e +". Reinicie o Programa.");
-}finally {
-    temperatura = new Double[n];
-    convertida = new Double[n];
 
-    for (int i = 0; i < temperatura.length; i++) {
-        System.out.printf("Digite o valor da %d temperatura: ", i + 1);
-        temp = sc.nextDouble();
-        temperatura[i] = temp;
-        convertida[i] = converterTemperatura(temp, entradaT, saidaT);
-        System.out.printf("Valor digitado em %c: %.2f º%c%n", entradaT, temp, entradaT);
-        System.out.printf("Valor convertido para %c: %.2f º%c%n", saidaT, convertida[i], saidaT);
-    }
-    for (int i = 0; i < convertida.length; i++) {
-        somaEntrada += temperatura[i];
-        somaSaida += convertida[i];
-    }
-    mediaEntrada = somaEntrada / n;
-    mediaSaida = somaSaida / n;
-    System.out.printf("Media das Temperaturas de entrada: %.2f %c%n", mediaEntrada, entradaT);
-    System.out.printf("Media das Temperaturas de saida: %.2f %c%n", mediaSaida, saidaT);
-}
-    }
+                System.out.println("Digite a quantidade de entradas as serem transformadas:");
+                n = sc.nextInt();
+                if (n > 0) {
+                    try {
+                System.out.println("Digite o tipo de temperatura para ser convertido(C/F/K): ");
+                entradaT = sc.next().charAt(0);
+                System.out.println("Digite o tipo de temperatura para qual deseja converter(C/F/K): ");
+                saidaT = sc.next().charAt(0);
+            } catch(Throwable e){
+                System.out.println("erro do tipo " + e + ". Reinicie o Programa.");
+            }finally{
+                temperatura = new Double[n];
+                convertida = new Double[n];
+                if ((entradaT == 'c' || entradaT == 'C') || (entradaT == 'f' || entradaT == 'F') || (entradaT == 'k' || entradaT == 'K')
+                        || (saidaT == 'c' || saidaT == 'C') || (saidaT == 'f' || saidaT == 'F') || (saidaT == 'k' || saidaT == 'K')) {
+                    for (int i = 0; i < temperatura.length; i++) {
+                        System.out.printf("Digite o valor da %d temperatura: ", i + 1);
+                        temp = sc.nextDouble();
+                        temperatura[i] = temp;
+                        convertida[i] = converterTemperatura(temp, entradaT, saidaT);
+                        System.out.printf("Valor digitado em %c: %.2f º%c%n", entradaT, temp, entradaT);
+                        System.out.printf("Valor convertido para %c: %.2f º%c%n", saidaT, convertida[i], saidaT);
+                    }
+                    for (int i = 0; i < convertida.length; i++) {
+                        somaEntrada += temperatura[i];
+                        somaSaida += convertida[i];
+                    }
+                } else {
+                    System.out.println("Erro de entrada Reinicie o Programa.");
+                    return;
+                }
+                mediaEntrada = somaEntrada / n;
+                mediaSaida = somaSaida / n;
+                System.out.printf("Media das Temperaturas de entrada: %.2f º%c%n", mediaEntrada, entradaT);
+                System.out.printf("Media das Temperaturas de saida: %.2f º%c%n", mediaSaida, saidaT);
+            }
+        }else{
+            System.out.println("Erro de entrada de dado. Reinicie o programa.");
+            return;
+            }
+        }
+
 
     private static Double converterTemperatura(Double temp, char entradaT, char saidaT) {
         switch (entradaT) {
@@ -70,6 +82,9 @@ try {
                     return kelvinParaFahrenheit(temp);
                 }
                 break;
+            default:
+                System.out.println("erro de entrada");
+                return temp = 0.0D;
         } return temp;
     }
     private static final Double FATOR1 =  9.0D / 5.0D;
